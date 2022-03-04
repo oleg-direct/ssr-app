b75e8b7157121dd7c2fc74d29bd601ea71d917c3
Oleg <sophisticated.kitesurfer@gmail.com>
2022-03-04 13:28:51 +1300

    Prod test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">First Feature!</a>
+          Welcome to <a href="https://nextjs.org">Prod Test!</a>
         </h1>
 
         <p className={styles.description}>
